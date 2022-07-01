5f5654d98905a6004c084ff0325bf5bd4b3fc60b
aCarinato <alessandro.carinato@gmail.com>
2022-07-01 21:26:15 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+function HomePage() {
+  const [messageFromServer, setMessageFromServer] = useState('');
+
+  const fetchMessage = async () => {
+    let message;
+    try {
+      const res = await axios.get(`${process.env.NEXT_PUBLIC_API}/products`);
+
+      message = res.data.message;
+      setMessageFromServer(message);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchMessage();
+  }, []);
+
+  return <div>{messageFromServer}</div>;
+}
+
+export default HomePage;
