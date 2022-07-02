c954d631bea0d75267c0fc7078227a1ea438544a
aCarinato <alessandro.carinato@gmail.com>
2022-07-02 10:21:57 +0200

    loading state

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,18 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 function HomePage() {
+  const [isLoading, setIsLoading] = useState(false);
   const [messageFromServer, setMessageFromServer] = useState('');
 
   const fetchMessage = async () => {
     let message;
     try {
+      setIsLoading(true);
       const res = await axios.get(`${process.env.NEXT_PUBLIC_API}/products/`);
 
       message = res.data.message;
       setMessageFromServer(message);
+      setIsLoading(false);
     } catch (err) {
       console.log(err);
     }
@@ -20,7 +23,7 @@ function HomePage() {
     fetchMessage();
   }, []);
 
-  return <div>{messageFromServer}</div>;
+  return <div>{isLoading ? <div>Loading</div> : messageFromServer}</div>;
 }
 
 export default HomePage;
