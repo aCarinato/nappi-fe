7b965a8e4b76ae74a64989d8455149ba3e32c153
aCarinato <alessandro.carinato@gmail.com>
2022-07-06 08:32:27 +0200

    general layout

--- components/layout/MainHeader.js
+++ components/layout/MainHeader.js
@@ -1,10 +1,45 @@
 import classes from './MainHeader.module.css';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import { useEffect, useRef, useState } from 'react';
+
+// import OutsideAlerter from './OutsiderAlert';
 
 function MainHeader() {
   const router = useRouter();
   const { locales, locale } = router;
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  const node = useRef();
+  console.log(node);
+
+  // Track events outside scope
+  const clickOutside = (e) => {
+    // console.log(node.current);
+    if (
+      node.current &&
+      node.current !== null &&
+      node.current.contains(e.target)
+    ) {
+      // inside click
+      console.log('clicked inside');
+      return;
+    }
+    // outside click
+    console.log('clicked outside scope');
+    setIsVisible(false);
+  };
+
+  // Do something after component renders
+  useEffect(() => {
+    document.addEventListener('mousedown', clickOutside);
+
+    // clean up function before running new effect
+    return () => {
+      document.removeEventListener('mousedown', clickOutside);
+    };
+  }, []);
 
   return (
     <header className={classes.container}>
@@ -22,17 +57,33 @@ function MainHeader() {
           <div className={classes['container-item-label']}>Kart</div>
         )}
         <div className={classes['container-item-label']}>Login</div>
-        <div className={classes['container-item-label']}>
-          {locales.map((l, i) => {
-            const { pathname, query, asPath } = router;
-            return (
-              <span key={i} className={classes.lang}>
-                <Link href={{ pathname, query }} as={asPath} locale={l}>
-                  <a>{l}</a>
-                </Link>
-              </span>
-            );
-          })}
+        <div className={classes['container-item-locales']}>
+          <div
+            className={classes['container-item-locale']}
+            onClick={() => setIsVisible((current) => !current)}
+          >
+            {locale}
+          </div>
+          {isVisible && (
+            <div ref={node} className={classes['container-flex-vertical']}>
+              {locales.map((l, i) => {
+                const { pathname, query, asPath } = router;
+                if (l !== locale) {
+                  return (
+                    <span
+                      onClick={() => setIsVisible((current) => !current)}
+                      key={i}
+                      className={classes.lang}
+                    >
+                      <Link href={{ pathname, query }} as={asPath} locale={l}>
+                        <a>{l}</a>
+                      </Link>
+                    </span>
+                  );
+                }
+              })}
+            </div>
+          )}
         </div>
       </div>
     </header>
